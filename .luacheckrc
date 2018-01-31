a25ad236ae5368950bdb133aab64169724ebdd28
Lucien Greathouse <me@lpghatguy.com>
2018-01-31 15:29:04 -0800

    Start implementing tests, add lemur and testez dependencies

--- .luacheckrc
+++ .luacheckrc
@@ -1,23 +1,6 @@
 stds.roblox = {
-	globals = {
-		"game"
-	},
 	read_globals = {
-		-- Roblox globals
-		"script",
-
-		-- Extra functions
-		"tick", "warn", "spawn",
-		"wait", "settings", "typeof",
-
-		-- Types
-		"Vector2", "Vector3",
-		"Color3",
-		"UDim", "UDim2",
-		"Rect",
-		"CFrame",
-		"Enum",
-		"Instance",
+		"script", "spawn", "warn", "Instance",
 	}
 }
 
--- lib/Promise.spec.lua
+++ lib/Promise.spec.lua
@@ -1,1 +1,41 @@
-local Promise = require(script.Parent.Promise)
+return function()
+	local Promise = require(script.Parent.Promise)
+
+	describe("Promise.new", function()
+		it("should pass resolve and reject to the callback", function()
+			local promiseResolve
+			local promiseReject
+			local callCount = 0
+
+			local promise = Promise.new(function(resolve, reject)
+				callCount = callCount + 1
+				promiseResolve = resolve
+				promiseReject = reject
+			end)
+
+			expect(promise).to.be.ok()
+			expect(promiseResolve).to.be.a("function")
+			expect(promiseReject).to.be.a("function")
+			expect(callCount).to.equal(1)
+		end)
+
+		it("should resolve synchronously", function()
+			local promiseResolve
+			local callCount = 0
+
+			local promise = Promise.new(function(resolve)
+				callCount = callCount + 1
+				promiseResolve = resolve
+			end)
+
+			expect(promise._status).to.equal(Promise.Status.Started)
+
+			promiseResolve(6)
+
+			expect(promise._status).to.equal(Promise.Status.Resolved)
+			expect(promise._value).to.be.a("table")
+			expect(#promise._value).to.equal(1)
+			expect(promise._value[1]).to.equal(6)
+		end)
+	end)
+end
