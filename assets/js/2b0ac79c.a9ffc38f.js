50b8dbfa2564ca31dc94075bc774173845e0c9a9
Eryn Lynn <eryn@eryn.io>
2021-09-30 18:11:15 -0400

    Built and published by Moonwave

---
+++ assets/js/2b0ac79c.a9ffc38f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[381],{23424:function(e,n,a){a.r(n),a.d(n,{default:function(){return u}});var s=a(5977),t=a(67294);function u(e){var n=e.allLuaClassNames,a=e.pluginOptions;return t.createElement(s.l_,{to:a.baseUrl+"api/"+n[0]})}}}]);
